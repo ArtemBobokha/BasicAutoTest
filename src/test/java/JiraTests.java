c46013243198f0e7dd74068092960a8bd9299fb5
ArtemBobokha <34837022+ArtemBobobkha@users.noreply.github.com>
2018-01-15 23:56:56 +0200

    Finish CreateTask test

--- src/test/java/JiraTests.java
+++ src/test/java/JiraTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.*;
 import org.testng.Assert;
 
 
+
 public class JiraTests {
     public static WebDriver browser;
 
@@ -33,10 +34,11 @@ public class JiraTests {
     }
 
     @Test
-    public static void CorrectLogIn(){
+    public static void CorrectLogIn() throws InterruptedException {
         browser.findElement(By.cssSelector("input[name=os_username]")).sendKeys("abobokha");
         browser.findElement(By.cssSelector("input[name=os_password]")).sendKeys("4159692125");
         browser.findElement(By.cssSelector("input[name='login']")).click();
+        Thread.sleep(3000);
         //Check user name
         String Name = browser.findElement(By.cssSelector("a[id='header-details-user-fullname']")).getAttribute("data-displayname");
         Assert.assertEquals(Name, "Artem Bobokha");
@@ -47,11 +49,22 @@ public class JiraTests {
         browser.findElement(By.cssSelector("input[name=os_username]")).sendKeys("abobokha");
         browser.findElement(By.cssSelector("input[name=os_password]")).sendKeys("4159692125");
         browser.findElement(By.cssSelector("input[name='login']")).click();
+        browser.findElement(By.id("find_link")).click();
+        Thread.sleep(1000);
+        browser.findElement(By.id("filter_lnk_reported_lnk")).click();
+        Thread.sleep(3000);
+        String issueBefore = browser.findElement(By.className("results-count-total")).getText();
+        Integer  countIssueBefore = Integer.parseInt(issueBefore);
+        String title = "Fatal error during enroll device";
         browser.findElement(By.id("create_link")).click();
         Thread.sleep(3000);
-        browser.findElement(By.cssSelector("input[name=summary]")).sendKeys("Fatal error during enroll device");
+        browser.findElement(By.cssSelector("input[name=summary]")).sendKeys(title);
         browser.findElement(By.cssSelector("input[id='create-issue-submit']")).click();
-        Thread.sleep(7000);
+        Thread.sleep(4000);
+        browser.findElement(By.className("aui-iconfont-refresh-small")).click();
+        String issueAfter = browser.findElement(By.className("results-count-total")).getText();
+        Integer  countIssueAfter = Integer.parseInt(issueAfter)-1;
+        Assert.assertEquals(countIssueBefore  ,countIssueAfter);
 
     }
 
